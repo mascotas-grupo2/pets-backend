a919f8e54910a5f187f9c1a0f77c46c3e81b2455
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2026-05-15 11:15:45 -0300

    Agregada entidad y migracion de adopción

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,15 +2,23 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Pet } from "./entity/Pet.js";
 import { User } from "./entity/User.js";
+import { Adoption } from "./entity/Adoption.js";
 import { InitPetTable1744402212000 } from "./migration/1744402212000-Init.js";
 import { AddUser1744402213000 } from "./migration/1744402213000-AddUser.js";
 import { AddAuthTokens1744402214000 } from "./migration/1744402214000-AddAuthTokens.js";
 import { AddPetCoordinates1744402215000 } from "./migration/1744402215000-AddPetCoordinates.js";
+import { AddAdoption1744402216000 } from "./migration/1744402216000-AddAdoption.js";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
-  entities: [Pet, User],
-  migrations: [InitPetTable1744402212000, AddUser1744402213000, AddAuthTokens1744402214000, AddPetCoordinates1744402215000],
+  entities: [Pet, User, Adoption],
+  migrations: [
+    InitPetTable1744402212000,
+    AddUser1744402213000,
+    AddAuthTokens1744402214000,
+    AddPetCoordinates1744402215000,
+    AddAdoption1744402216000,
+  ],
   synchronize: false,
 });
