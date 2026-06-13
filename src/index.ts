bf1a77689c4e8063ab39652858f8ed5fea8102ef
cberto <bertolanicami@gmail.com>
2026-06-13 19:24:58 -0300

    feat: refactor server initialization to use HTTP server for Socket.IO integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
+import http from "http";
 import { AppDataSource } from "./data-source.js";
 import { app } from "./app.js";
 import { bootstrapAdmins } from "./lib/bootstrap-admins.js";
+import { initRealtime } from "./lib/realtime.js";
 
 const port = Number(process.env.PORT) || 3001;
 
@@ -9,7 +11,10 @@ AppDataSource.initialize()
   .then(async () => {
     await AppDataSource.runMigrations();
     await bootstrapAdmins();
-    app.listen(port, () => {
+    // Server HTTP propio para colgar Socket.IO (push de notificaciones).
+    const server = http.createServer(app);
+    initRealtime(server);
+    server.listen(port, () => {
       console.log(`Servidor corriendo en http://localhost:${port}`);
     });
   })
