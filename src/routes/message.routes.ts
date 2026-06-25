80c5f3694746a5348512a9c89b17af28a841539f
cberto <bertolanicami@gmail.com>
2026-06-24 21:29:02 -0300

    Add admin alerts endpoint to message routes

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { requireAuth } from "../lib/auth.js";
-import { getConversation, getInbox, sendMessage, getAdminConversations, deleteMessage } from "../controllers/message.controller.js";
+import { getConversation, getInbox, sendMessage, getAdminConversations, deleteMessage, getAdminAlerts } from "../controllers/message.controller.js";
 import { requireAdmin } from "../lib/auth.js";
 import { single, multerErrorHandler } from "../middleware/upload.js";
 
@@ -10,6 +10,7 @@ messageRouter.use(requireAuth);
 
 messageRouter.get("/inbox", getInbox);
 messageRouter.get("/admin/inbox", requireAdmin, getAdminConversations);
+messageRouter.get("/admin/alerts", requireAdmin, getAdminAlerts);
 messageRouter.get("/conversation/:userId", getConversation);
 messageRouter.post("/", single("photo"), multerErrorHandler, sendMessage);
 messageRouter.delete("/:id", deleteMessage);
